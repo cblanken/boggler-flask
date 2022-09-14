129c002c85cf4eeb365432e907de9f70b1f6ae89
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-09-14 11:06:01 -0400

    Replace words_table.css with Bootstrap classes and update table icons

---
+++ static/js/words_table.js
@@ -0,0 +1,7 @@
+// Apply table icons
+window.icons = {
+    columns: "bi-sliders",
+    fullscreen: "bi-arrows-fullscreen",
+    paginationSwitchDown: "bi-arrows-expand",
+    paginationSwitchUp: "bi-arrows-collapse",
+}
