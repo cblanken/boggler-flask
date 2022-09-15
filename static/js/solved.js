88f9fc7d381976349315cc92ebc4134199be10b2
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-09-15 10:08:18 -0400

    Update diagonal path highlights and fix onUncheck bug
    - Diagonal paths now focus the center of starting and ending cells with
      pointAnchors
    - onUncheck removed references to without clearing list causing errors
      on followup selection of table rows

--- static/js/solved.js
+++ static/js/solved.js
@@ -1,7 +1,6 @@
 let $table = $("#word-table");
 let board_cells = document.querySelectorAll(".board-cell");
 let path_buttons = document.querySelectorAll("#test-table tr .path-cell button")
-let arrows = []
 
 // Add table filter board click events
 window.operateEvents = {
@@ -31,33 +30,73 @@ function toggle_path_highlight(path) {
 }
 
 function draw_path_connections(path) {
-    arrows = []
+    let arrows = [];
+    let arrow_params = {
+        path: "straight",
+        color: "#09008980",
+        dash: {
+            animation: true,
+            gap: 8,
+        },
+        size: 6,
+    }
+
     for (i=1; i < path.length; i++) {
-        // TODO: add alternate handling for diagonal links (from center-to-center)
         cell1 = path[i-1]
         cell2 = path[i]
         c1 = document.querySelector(`#board [data-pos=\"${cell1[0]},${cell1[1]}\"]`)
         c2 = document.querySelector(`#board [data-pos=\"${cell2[0]},${cell2[1]}\"]`)
-        arrows.push(new LeaderLine(
-            LeaderLine.areaAnchor(c1, {
-                x: "25%", y: "25%", width: "50%", height: "50%", radius: 20, color: "#FFFFFF00"
-            }),
-            LeaderLine.areaAnchor(c2, {
-                x: "25%", y: "25%", width: "50%", height: "50%", color: "#FFFFFF00"
-            }),
-            {
-                path: "straight",
-                color: "#09008980",
-                dash: {
-                    animation: true,
-                    gap: 8,
-                },
-                size: 6,
-            },
-        ))
+
+        if (cell1[0] === cell2[0] ||
+            cell1[1] === cell2[1] 
+        ) { // Draw horizontal and vertical arrows between cells
+            arrows.push(new LeaderLine(
+                LeaderLine.areaAnchor(c1, {
+                    x: "25%", y: "25%", width: "50%", height: "50%", color: "#FFFFFF00"
+                }),
+                LeaderLine.areaAnchor(c2, {
+                    x: "25%", y: "25%", width: "50%", height: "50%", color: "#FFFFFF00"
+                }),
+                arrow_params
+            ))
+        } else { // Draw diagonal arrows between cells
+            if (cell2[1] < cell1[1]) { // c2 left of c1
+                x1 = "25%"
+                x2 = "75%"
+            } else { // c2 right of c1
+                x1 = "75%"
+                x2 = "25%"
+            }
+            if (cell2[0] < cell1[0]) { // c2 above c1
+                y1 = "25%" 
+                y2 = "75%" 
+            } else { // c2 below c1
+                y1 = "75%"
+                y2 = "25%"
+            }
+
+            arrows.push(new LeaderLine(
+                LeaderLine.pointAnchor(c1, {
+                    x: x1, y: y1
+                }),
+                LeaderLine.pointAnchor(c2, {
+                    x: x2, y: y2
+                }),
+                arrow_params
+            ))
+        }
     }
 
     return arrows
+}
+
+function remove_arrows(arrows) {
+    console.log(arrows)
+    if (arrows.length > 0) {
+        arrows.forEach(arrow => {
+            if(arrow !== undefined) { arrow.remove(); } 
+        })
+    }
 }
 
 function get_path_from_string(path_string) {
@@ -92,18 +131,21 @@ board_cells.forEach( cell => {
 })
 
 // Add board highlight table click events
+let active_arrows = []
 $table.bootstrapTable({
     onCheck: function (row, element) {
         // TODO: implement correct API query to avoid doing this weird parsing
         // TODO: current solution only works for single digit indices
         unhighlight_board()
-        arrows.forEach(arrow => arrow.remove() )
+        console.log("onCheck", active_arrows)
+        remove_arrows(active_arrows)
         path = get_path_from_string(row.path)
-        console.log(path)
         toggle_path_highlight(path)
-        draw_path_connections(path)
+        active_arrows = draw_path_connections(path)
     },
     onUncheck: function (row, element) {
         unhighlight_board()
+        remove_arrows(active_arrows)
+        active_arrows = []
     }
 })
