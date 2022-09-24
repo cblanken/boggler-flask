bde34eca6fc29d05bd9f192034c7b0dfbb30bcfd
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-09-23 23:36:52 -0400

    Add board randomizer (not dice based)

--- static/js/solver.js
+++ static/js/solver.js
@@ -16,8 +16,6 @@ solveBtn.addEventListener("click", x => {
 // Board resizing
 let sizeSelect = document.getElementById("sizeSelect")
 sizeSelect.addEventListener("change", e => {
-    console.log('boardSize:', sizeSelect.value)
-
     // Delete old board
     board = document.getElementById("board")
     board.textContent = ""
@@ -40,5 +38,21 @@ sizeSelect.addEventListener("change", e => {
             row_div.append(cell_div)
         }
         board.append(row_div)
+    }
+})
+
+// Randomize board button
+let randomBoardBtn = document.getElementById("randomBoardBtn")
+randomBoardBtn.addEventListener("click", e => {
+    // TODO: pull random dice from board_randomizer.py
+    // alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','qu','r','s','t','u','v','w','x','y','z']
+    // alphabet with matching 4x4 dice distribution
+    alphabet = ["a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "b", "c", "c", "c", "d", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", "h", "h", "i", "i", "i", "i", "i", "i", "i", "j", "k", "k", "l", "l", "l", "l", "l", "m", "m", "m", "n", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "p", "p", "p", "qu", "r", "r", "r", "r", "s", "s", "s", "s", "s", "t", "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "y", "z"]
+    for (let row = 0; row < sizeSelect.value; row++) {
+        for (let col = 0; col < sizeSelect.value; col++) {
+            let index = parseInt(Math.random() * alphabet.length) % alphabet.length
+            letter_input = document.querySelector(`.board-cell[data-pos='${row},${col}']`)
+            letter_input.firstElementChild.value = alphabet[index]
+        }
     }
 })
