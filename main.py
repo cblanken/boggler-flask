6f24492fb7ed26cad06cb16155f4017847c5aec2
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-09-30 16:15:09 -0400

    Include werkzeug proxy_fix middleware for nginx reverse proxy build

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 app = create_app()
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
