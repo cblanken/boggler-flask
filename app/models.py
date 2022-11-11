cdc9271d74bd2eee561ee7f9b5a7b9af1543ee21
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-11-11 14:29:40 -0500

    Import db from app for models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
+from . import db
 
-db = SQLAlchemy()
 class Dictionary(db.Model):
     __tablename__ = "dictionaries"
     id = db.Column(db.Integer, primary_key=True)
