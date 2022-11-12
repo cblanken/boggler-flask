75c123cf07f55aa738fe60ea205f95b8bc98d28a
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-11-11 21:53:13 -0500

    Expand configs to use environment variables

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,5 @@
 """Config layer to apply configurations based on environment type"""
+import sys
 import os
 import secrets
 import string
@@ -9,6 +10,7 @@ def get_secret():
     """Generate random alphanumeric secret"""
     return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(32))
 
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or get_secret()
 
@@ -18,10 +20,28 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    DB_HOST = os.environ.get("DEV_DB_HOST") or "localhost"
+    DB_PORT = os.environ.get("DEV_DB_PORT") or "5555"
+    DB_USER = os.environ.get("DEV_DB_USER") or "postgres"
+    DB_PASS = os.environ.get("DEV_DB_PASS") or "password"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/boggler?connect_timeout=10"
 
 class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    DB_HOST = os.environ.get("PROD_DB_HOST") or "localhost"
+    DB_PORT = os.environ.get("PROD_DB_PORT") or "5555"
+    DB_USER = os.environ.get("PROD_DB_USER") or input("Provide a database username to continue: ")
+    DB_PASS = os.environ.get("PROD_DB_PASS") or input("Provide a database password to continue: ")
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/boggler?connect_timeout=10"
+
     @classmethod
     def init_app(cls, app):
+        if cls.DB_USER is None or cls.DB_PASS is None:
+            print("Incomplete configuration. A database username and password must be provided via the PROD_DB_USER and PROD_DB_PASS environment variables", file=sys.stderr)
+            sys.exit()
+
         Config.init_app(app)
         app.wsgi_app = ProxyFix(
             app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
@@ -30,9 +50,11 @@ class ProductionConfig(Config):
 class CeleryConfig(Config):
     @staticmethod
     def create_celery():
+        REDIS_HOST = os.environ.get("REDIS_HOST") or "redis"
+        REDIS_PORT = os.environ.get("REDIS_PORT") or "6379"
         return Celery("boggler-celery",
-            broker="redis://redis:6379/0",
-            backend="redis://redis:6379/0",
+            broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+            backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
             include="app.board"
         )
 
