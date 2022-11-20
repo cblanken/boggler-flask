e868f0701a103c260f470ed0d2a83ce5809a1a5c
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-11-19 19:22:50 -0500

    Style light/dark mode toggle

--- app/static/js/header.js
+++ app/static/js/header.js
@@ -1,8 +1,31 @@
-let theme_toggle = document.querySelector(".theme-switch input[type='checkbox']")
-theme_toggle.addEventListener("change", e => {
-    if (e.target.checked) {
+let theme_toggle = document.querySelector("#theme-switch");
+let theme_toggle_slider = document.querySelector("#theme-switch .toggle-slider");
+let theme_toggle_checkbox = document.querySelector("#theme-switch input[type='checkbox']");
+theme_toggle.addEventListener("click", e => {
+    theme_toggle_checkbox.checked = !theme_toggle_checkbox.checked;
+    console.log(theme_toggle_slider)
+    if (theme_toggle_checkbox.checked) {
         document.documentElement.setAttribute("data-theme", "dark");
+        theme_toggle_slider.classList.add("toggle-slider-anim-dark")
+        change_theme("toggle-slider-anim-light");
     } else {
         document.documentElement.setAttribute("data-theme", "light");
+        theme_toggle_slider.classList.add("toggle-slider-anim-light")
+        change_theme("toggle-slider-anim-dark");
     }
 }, false);
+
+function debounce(func, timeout = 250) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args) }, timeout);
+    }
+}
+
+function change_theme(anim_class) {
+    debounce(() => {
+        theme_toggle_slider.classList.add(anim_class);
+    });
+    theme_toggle_slider.classList.remove(anim_class);
+}
